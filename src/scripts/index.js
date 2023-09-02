db48fbd9a83d80088b107a3fd382cbf0e2677e15
Анастасия <n060600@yandex.ru>
2023-09-02 22:52:37 +0500

    Add, delete and mark task. Without 4 buttons

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,23 +5,31 @@ const btnDeleteLast = document.querySelector('[name=btn-del-last]');
 const btnAddTask = document.querySelector('[name=btn-add]');
 const btnDeleteTask = document.querySelector('[name=btn-delete]');
 const tasksList = document.querySelector('.list');
+const taskTemplate = document.querySelector('.task-template');
+const taskTextInput = document.querySelector('.add__input')
 
 //функция привязки обработчика события клика по кнопке удаления 
 function bindDeleteButtonClickHandler(task) {
-    task.querySelector('.list__button-del').addEventListener('click', function (evt) {
+    task.querySelector('.task__button-del').addEventListener('click', function (evt) {
         task.remove();
     });
 };
 
-//функция привязки обработчика события клика по лайку 
+//функция привязки обработчика события клика по галочке 
 function bindMarkButtonClickHandler(task) {
-    task.querySelector('.list__button-comp').addEventListener('click', function (evt) {
-        evt.target.classList.toggle('list__button-comp_click');
+    task.querySelector('.task__button-comp').addEventListener('click', function (evt) {
+        evt.target.classList.toggle('task__button-comp_click');
+        task.classList.toggle('task_theme_comp');
+        if (task.classList.contains('task_theme_comp')) {
+            tasksList.append(task);
+        } else {
+            tasksList.prepend(task);
+        }
+        
     });
 };
 
-//форма отправки имени и инфо 
-
+//форма отправки задачи
 function handleEditFormSubmit(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
@@ -29,20 +37,21 @@ function handleEditFormSubmit(evt) {
     closeModalWindow(popupEditProfile);
 };
 
-//функция поиска картинки и её дабавления 
+//функция добавления задачи
 function createTask(taskText) {
-    const task = imgTemplate.querySelector('.list__item').cloneNode(true);
-    task.querySelector('.list__text').textContent = title;
+    const taskCopy = taskTemplate.content.cloneNode(true);
+    const task = taskCopy.querySelector('.task').cloneNode(true);
+    task.querySelector('.task__text').textContent = taskText;
     bindMarkButtonClickHandler(task);
     bindDeleteButtonClickHandler(task);
     return task;
 };
 
 //вызов функции добавления задачи через кнопку add 
-buttonAddTask.addEventListener('click', function (evt) {
+btnAddTask.addEventListener('click', function (evt) {
     evt.preventDefault();
-    const task = createTask(taskText.value);
+    const task = createTask(taskTextInput.value);
     tasksList.prepend(task);
-    taskText.value = '';
+    taskTextInput.value = '';
     // resetButtonSave(popupAddCard, popupValidation);
 });
